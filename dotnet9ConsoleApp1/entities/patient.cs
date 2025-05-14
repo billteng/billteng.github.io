c18b01d339af658c7098d1bb1606156fec846d64
Bill Teng <bill.hstpathways@hotmail.com>
2025-05-14 17:32:26 -0600

    updates

--- dotnet9ConsoleApp1/entities/patient.cs
+++ dotnet9ConsoleApp1/entities/patient.cs
@@ -1,31 +1,25 @@
-// patien entity class
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Text.Json.Serialization;
 
 namespace dotnet9ConsoleApp1.entities
 {
     public class Patient
     {
         [Key]
-        public int Id { get; set; }  // Primary key
+        public int Id { get; set; }
 
         [Required]
         [MaxLength(50)]
-        public required string FirstName { get; set; }
+        public string? FirstName { get; set; }
 
         [MaxLength(50)]
         public string? MiddleName { get; set; }
 
         [MaxLength(50)]
-        public required string LastName { get; set; }
+        [Required]
+        public string? LastName { get; set; }
 
         [Required]
-        public required DateTime BirthDate { get; set; }
+        public DateTime BirthDate { get; set; }
     }
 }
