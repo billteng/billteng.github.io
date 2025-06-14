3bd0aad34ffa355b522633b4c011011970a2430b
Bill Teng <bill.hstpathways@hotmail.com>
2025-06-14 15:27:40 -0600

    Update and deleted all

--- cognitive-services-speech-sdk-js-1.44.1/jest.setup.js
+++
@@ -1,6 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const nodeFetch = require("node-fetch");
-global.fetch = nodeFetch;
-global.Response = nodeFetch.Response;
-global.Headers = nodeFetch.Headers;
-global.Request = nodeFetch.Request;
--- cognitive-services-speech-sdk-js-1.44.1/src/common/IEventListener.ts
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT license.
-
-import { PlatformEvent } from "./PlatformEvent.js";
-
-export interface IEventListener<TEvent extends PlatformEvent> {
-    onEvent(e: TEvent): void;
-}
