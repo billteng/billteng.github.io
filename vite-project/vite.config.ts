57b1f424648b81304a2c828c99cc7d11aa859e85
Bill Teng <bill.hstpathways@hotmail.com>
2025-06-26 17:39:26 -0600

    update and using release

--- vite-project/vite.config.ts
+++ vite-project/vite.config.ts
@@ -1,4 +1,15 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+    base: './',
+    build: {
+        outDir: 'release', // default is dist, e.g., 'build', 'public', 'release', etc.
+        rollupOptions: {
+            output: {
+                entryFileNames: 'assets/[name].js',         // main JS file
+                chunkFileNames: 'assets/[name]-[hash].js',  // code-split JS
+                assetFileNames: 'assets/[name][extname]'    // CSS, images, etc.
+            }
+        }
+    }
 })
