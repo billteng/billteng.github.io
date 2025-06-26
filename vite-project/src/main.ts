4ee27d10df865dfbaf92b5358e7a852d8c769977
Bill Teng <bill.hstpathways@hotmail.com>
2025-06-26 17:14:35 -0600

    remove unworking component

--- vite-project/src/main.ts
+++ vite-project/src/main.ts
@@ -2,7 +2,7 @@ import './style.css'
 import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
-import './components/fcode-editor.ts'
+//import './components/fcode-editor.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
@@ -20,9 +20,6 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
       Click on the Vite and TypeScript logos to learn more
     </p>
     <hr>
-    <div>
-      <fcode-editor></fcode-editor>
-    </div>
   </div>
 `
 
